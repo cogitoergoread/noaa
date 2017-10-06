84c6c4207eb359310d7ee0dffbd121e6505b3615
Jozsef Varga <31209755+cogitoergoread@users.noreply.github.com>
2017-10-06 22:00:33 +0200

    Earthquake data load.
    data/ contains the database of earthquakes and a loader function for it.
    R/data.R contains the detailed description.

---
+++ R/proba.R
@@ -0,0 +1,20 @@
+library("readr")
+library("dplyr")
+library(lubridate)
+# Read data
+rdata <- function() {
+  read_tsv(file="e:/Muszi/tmp/signif.txt", col_names = TRUE)
+}
+
+eq_clean_data <- function( frame ){
+  frame %>%
+    mutate_at( funs(replace(., is.na(.), 1)) , .vars = c( "MONTH", "DAY")  ) %>%
+    mutate_at( funs(replace(., is.na(.), 0)) , .vars = c("YEAR", "HOUR", "MINUTE", "SECOND")  ) %>%
+    unite(date, YEAR, MONTH, DAY, HOUR, MINUTE, SECOND)  %>%
+    mutate(date=ymd_hms(date))
+
+  frame %>%
+    mutate_at( funs(replace(., is.na(.), 1)) , .vars = c( "MONTH", "DAY")  ) %>%
+    mutate_at( funs(replace(., is.na(.), 0)) , .vars = c("YEAR", "HOUR", "MINUTE", "SECOND")  ) %>%
+    mutate(date=make_datetime (year = YEAR, month = MONTH, day = DAY, hour = HOUR, min = MINUTE, sec = SECOND) )
+}
